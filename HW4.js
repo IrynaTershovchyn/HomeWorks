6eb7df2ae65403e83161fb78448d55cad00b3337
IrynaTershovchyn <tersovcinirina@gmail.com>
2024-11-14 02:33:24 +0200

    new file

--- HW4.js
+++ HW4.js
@@ -21,7 +21,6 @@ function checkProbabilityTheory(count) {
             oddNumber +=1;
         }
     }
-        
     let evenPercent = Math.round(100/rndArray.length * evenNumber);
     let oddPercent = Math.round(100 - evenPercent);
     console.log ("Кількість парних чисел: " + evenNumber);
